d1f0d18c02ab3c7486b8e82b3b7252b8de1a1f45
Mahpudin <pudinmah69@gmail.com>
2023-12-11 01:50:34 +0700

    Belajar Route

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// static
+Route::get('/hello',function(){
+    return "Hello World";
+});
+
+// parameter
+Route::get('/nama/{nama}',function($nama){
+    return "Tampilkan data nama berserta $nama";
+});
+
+// Route Group
+Route::prefix('/admin')->group(function(){
+
+   Route::get('/mahasiswa',function(){
+    echo "<h1>Daftar Mahasiswa</h1>";
+   });
+   Route::get('/dosen',function(){
+    echo "<h1>Daftar Dosen</h1>";
+   });
+   Route::get('/karyawan',function(){
+    echo "<h1>Daftar Karyawan</h1>";
+   });
+
+
+
+});
